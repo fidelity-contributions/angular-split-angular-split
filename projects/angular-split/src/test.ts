5b3c42c426deb29d6c06e84c7cd3ce7e2744ab9b
Dimri, Rahul <Rahul.Dimri@fmr.com>
2024-11-21 20:46:31 +0530

    Update test.ts
    Signed-off-by: Dimri, Rahul <Rahul.Dimri@fmr.com>

--- projects/angular-split/src/test.ts
+++ projects/angular-split/src/test.ts
@@ -1,17 +1,38 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'core-js/es7/reflect'
-import 'zone.js'
-import 'zone.js/testing'
-import { getTestBed } from '@angular/core/testing'
-import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing'
+//import 'core-js/es7/reflect'
+//import 'zone.js'
+//import 'zone.js/testing'
+//import { getTestBed } from '@angular/core/testing'
+//import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing'
 
-declare const require: any
+//declare const require: any
 
 // First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting())
+//getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting())
 // Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/)
+//const context = require.context('./', true, /\.spec\.ts$/)
 // And load the modules.
 context.keys().map(context)
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/*
+* This file is required by karma.conf.js and loads recursively all the .spec and framework files.
+*/
+import 'zone.js'; // Required for Angular
+import 'zone.js/testing'; // Required for Angular testing
+import {
+ getTestBed,
+ BrowserDynamicTestingModule,
+ platformBrowserDynamicTesting,
+} from '@angular/core/testing';
+// First, initialize the Angular testing environment.
+getTestBed().initTestEnvironment(
+ BrowserDynamicTestingModule,
+ platformBrowserDynamicTesting()
+);
+// Then, find all the test files.
+const context = (require as any).context('./', true, /\.spec\.ts$/);
+// And load the modules.
+context.keys().forEach(context);
